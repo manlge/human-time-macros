28a336abd0efcd4e6eb119752cd41c0738a71edf
Mark Liu <manlge168@gmail.com>
2022-12-23 16:49:40 +0800

    use human_time crate functions

--- src/lib.rs
+++ src/lib.rs
@@ -22,46 +22,7 @@ pub fn elapsed(_attr: TokenStream, func: TokenStream) -> TokenStream {
 
             let start = time::Instant::now();
             let fn_return_value = #fn_block;
-            {
-                fn human_time(d: Duration) -> String {
-                    human_time_with_format(
-                        d,
-                        |n, unit| format!("{}{}",n , unit),
-                        |acc, item| format!("{},{}", acc, item),
-                    )
-                }
-
-                fn human_time_with_format<F, F1>(d: Duration, time_fmt: F, res_fmt: F1) -> String
-                where
-                    F: Fn(u128, &str) -> String,
-                    F1: Fn(String, String) -> String,
-                {
-                    let mut map: Vec<(u128, &str)> = Vec::new();
-                    let mut ms = d.as_millis();
-                    for (unit, n_ms) in [
-                        ("d", 86400000),
-                        ("h", 3600000),
-                        ("m", 60000),
-                        ("s", 1000),
-                        ("ms", 1),
-                    ] {
-                        map.push((ms / n_ms, unit));
-                        ms %= n_ms;
-                    }
-
-                    match map
-                        .into_iter()
-                        .filter_map(|(n, u)| if n > 0 { Some(time_fmt(n, u)) } else { None })
-                        .reduce(res_fmt)
-                    {
-                        Some(val) => val,
-                        None => time_fmt(0, "ms"),
-                    }
-                }
-
-                println!("fn {} costs {}", #fn_name_str, human_time(start.elapsed()));
-
-            }
+            println!("fn {} costs {}", #fn_name_str, human_time::human_time(start.elapsed()));
             fn_return_value
         }
     };
