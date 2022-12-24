6b24b7d44fad062344a691d603524dfe7068addf
Mark Liu <manlge168@gmail.com>
2022-12-24 10:49:17 +0800

    support const unsafety  abi functions

--- src/lib.rs
+++ src/lib.rs
@@ -6,24 +6,27 @@ use syn::ItemFn;
 #[proc_macro_attribute]
 pub fn elapsed(_attr: TokenStream, func: TokenStream) -> TokenStream {
     let func = parse_macro_input!(func as ItemFn);
-    let fn_vis = &func.vis;
-    let fn_block = &func.block;
+    let vis = &func.vis;
+    let block = &func.block;
 
-    let fn_sig = func.sig;
-    let fn_asyncnes = &fn_sig.asyncness;
-    let fn_ident = &fn_sig.ident;
-    let fn_name_str = fn_ident.to_string();
-    let fn_generics = &fn_sig.generics;
-    let fn_inputs = &fn_sig.inputs;
-    let fn_output = &fn_sig.output;
+    let signature = func.sig;
+    let asyncness = &signature.asyncness;
+    let constness = &signature.constness;
+    let unsafety = &signature.unsafety;
+    let abi = &signature.abi;
+    let ident = &signature.ident;
+    let name_str = ident.to_string();
+    let generics = &signature.generics;
+    let inputs = &signature.inputs;
+    let output = &signature.output;
 
     let new_fn = quote! {
-        #fn_vis #fn_asyncnes fn #fn_ident #fn_generics(#fn_inputs) #fn_output {
+        #vis #constness #asyncness #unsafety #abi fn #ident #generics(#inputs) #output {
             use std::time;
 
             let start = time::Instant::now();
-            let fn_return_value = #fn_block;
-            println!("fn {} costs {}", #fn_name_str, human_time::human_time(start.elapsed()));
+            let fn_return_value = #block;
+            println!("fn {} costs {}", #name_str, human_time::human_time(start.elapsed()));
             fn_return_value
         }
     };
